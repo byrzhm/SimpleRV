b48270b21e0c91ea8dd62dc972b0182ae6733acb
zhm_mac <zhm1019@qq.com>
2024-07-24 23:56:37 +0800

    update

---
+++ software/151_library/types.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+
+typedef char  int8_t;
+typedef short int16_t;
+typedef int   int32_t;
+
+#define NULL 0
+
+#define TRUE  1
+#define FALSE 0
+
+#endif
